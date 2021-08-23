8dd7d6dc7f026e0a67fdbbe020fc60fbce3cf30d
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-08-23 12:04:50 +0900

    chore: set site metadata

--- packages/www/gatsby-config.js
+++ packages/www/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`
+    title: `そこんところ工房`,
+    description: `いろいろやってるサークル、そこんところ工房のWebsiteです。内側に何人かいます。`,
+    author: `@skntkr_factory`,
+    siteUrl: `https://www.sokontokoro-factory.net/`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,13 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/assets/image/hammer.png`
       }
     },
     `gatsby-plugin-gatsby-cloud`
